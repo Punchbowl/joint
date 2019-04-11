7e244aa4b0ee4b16bae144c89e11b3bfede55817
Blake Carlson <blc@punchbowl.com>
2019-04-11 12:17:54 -0400

    add readonly joint attachment support

--- lib/joint/class_methods.rb
+++ lib/joint/class_methods.rb
@@ -10,10 +10,12 @@ module Joint
 
       self.attachment_names = attachment_names.dup.add(name)
 
-      after_save     :save_attachments
-      before_save    :nullify_nil_attachments_attributes
-      after_save     :destroy_nil_attachments
-      before_destroy :destroy_all_attachments
+      unless options[:readonly]
+        after_save     :save_attachments
+        before_save    :nullify_nil_attachments_attributes
+        after_save     :destroy_nil_attachments
+        before_destroy :destroy_all_attachments
+      end
 
       key :"#{name}_id",   ObjectId
       key :"#{name}_name", String
@@ -30,21 +32,25 @@ module Joint
         def #{name}?
           !nil_attachments.has_key?(:#{name}) && send(:#{name}_id?)
         end
-
-        def #{name}=(file)
-          if file.nil?
-            nil_attachments[:#{name}] = send("#{name}_id")
-            assigned_attachments.delete(:#{name})
-          else
-            send("#{name}_id=", BSON::ObjectId.new) if send("#{name}_id").nil?
-            send("#{name}_name=", Joint::FileHelpers.name(file))
-            send("#{name}_size=", Joint::FileHelpers.size(file))
-            send("#{name}_type=", Joint::FileHelpers.type(file))
-            assigned_attachments[:#{name}] = file
-            nil_attachments.delete(:#{name})
-          end
-        end
       EOC
+
+      unless options[:readonly]
+        self.class_eval <<-EOC
+          def #{name}=(file)
+            if file.nil?
+              nil_attachments[:#{name}] = send("#{name}_id")
+              assigned_attachments.delete(:#{name})
+            else
+              send("#{name}_id=", BSON::ObjectId.new) if send("#{name}_id").nil?
+              send("#{name}_name=", Joint::FileHelpers.name(file))
+              send("#{name}_size=", Joint::FileHelpers.size(file))
+              send("#{name}_type=", Joint::FileHelpers.type(file))
+              assigned_attachments[:#{name}] = file
+              nil_attachments.delete(:#{name})
+            end
+          end
+        EOC
+      end
     end
   end
 end
